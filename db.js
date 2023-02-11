e089a31432e7dabac20e0f09dbf74105590b60f6
Rebooting-Me <arsharma@ucsd.edu>
2023-02-11 10:49:48 -0800

    created user-specific schemas and their models

--- db.js
+++ db.js
@@ -23,3 +23,48 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('Connected to MongoDB');
 });
+
+//instantiated a schema
+const Schema = mongoose.Schema;
+
+//Created a homeowner schema
+const homeownerSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+//Created a homeowner model
+const Homeowner = mongoose.model('Homeowner', homeownerSchema);
+module.exports = Homeowner;
+
+//Created a contractor schema
+const contractorSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+//Created a contractor model
+const Contractor = mongoose.model('Contractor', contractorSchema);
+module.exports = Contractor;
