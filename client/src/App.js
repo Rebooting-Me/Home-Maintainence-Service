ab4539183e9ad1219982d58853f0e1a6d64a6c43
Shawn Hu <s1hu@ucsd.edu>
2023-02-14 23:58:00 -0800

    Implemented hero section of landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './components/landing/Navbar/Navbar';
+import Hero from './components/landing/Hero/Hero';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
---
+++ client/src/components/landing/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './Hero.module.css'
+import Family from '../../../assets/family.svg'
+
+const Hero = () => {
+  return (
+    <div className={`${styles.heroWrapper} center`}>
+        <div className={styles.heroInner}>
+            <h2 className={styles.headerText}>
+                Experience hassle-free home upkeep with CasaCare®
+            </h2>
+            <div className={styles.slogan}>
+                <p>Connecting you to a network of expert contractors for all your repair and maintenance needs.</p>
+            </div>
+            <div className={styles.email}>
+                <input type='text' placeholder="Enter your email" className={styles.emailInput}></input>
+                <input type='button' value='Sign Up' className={styles.signUp}></input>
+            </div>
+        </div>
+        <div className={styles.heroImage}>
+            <img src={Family} alt=''/>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
