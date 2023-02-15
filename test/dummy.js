a728acc61c12f0696e900933c63535fa9133d565
and-le <a1le@ucsd.edu>
2023-02-14 19:29:30 -0800

    Add dummy test files; update package.json

---
+++ test/dummy.js
@@ -0,0 +1,15 @@
+/**
+ * Dummy source file demonstrating how to use Jest for testing.
+ */
+
+/**
+ * The dummy function to be tested. 
+ * @param {*} x 
+ * @param {*} y 
+ * @returns the sum of x and y
+ */
+function dummySum(x, y) {
+    return x + y;
+}
+
+module.exports = dummySum;
