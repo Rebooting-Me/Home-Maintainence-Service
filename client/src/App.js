3de4d7d2d5dad5f23d62c5043142dc09b33e94cb
Shawn Hu <s1hu@ucsd.edu>
2023-02-16 13:55:03 -0800

    Implemented features in landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/landing/Navbar/Navbar';
 import Hero from './components/landing/Hero/Hero';
+import Features from './components/landing/Features/Features';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Hero/>
+      <Features/>
     </div>
   );
 }
