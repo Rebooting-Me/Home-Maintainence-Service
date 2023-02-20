5555dd341cc69c55448519aa14e70c7ab6babfc9
Sanjana Balamurugan <sanjanabalamurugan@Sanjanas-MacBook-Air.local>
2023-02-19 17:20:51 -0800

    Remove frontend

--- test/dummy.test.js
+++
@@ -1,8 +0,0 @@
-/**
- * Dummy test file demonstrating how to use Jest for testing.
- */
-const dummySum = require('./dummy');
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(dummySum(1, 2)).toBe(3);
-});
