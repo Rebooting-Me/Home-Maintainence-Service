318f1be6e481fa1a4f3adacd3fda53780a0a2ede
Rebooting-Me <arsharma@ucsd.edu>
2023-02-22 17:43:56 -0800

    created new controller and updated the newListing route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,3 @@
-
 require('dotenv').config();
 const { login, signup } = require('../models/helper');
 const jwt = require('jsonwebtoken');
--- backend/routes/newListing.js
+++ backend/routes/newListing.js
@@ -2,19 +2,12 @@ const express = require('express');
 const router = express.Router();
 const requireAuth = require('../middleware/requireAuth');
 const { authenticateHomeowner } = require('../middleware/homeOwnerAuth');
-const { createProjectListing } = require('../models/helper');
+const { createListing } = require('../controllers/newListingController');
+
+//require authentication
+router.use(requireAuth);
 
 // Create a new listing
-router.post('/newListing', requireAuth, authenticateHomeowner, async (req, res) => {
-  try {
-    const { title, description, city, state, zip_code, serviceId } = req.body;
-    const ownerId = req.user._id;
-    const createdListing = await createProjectListing({ title, description, city, state, zip_code, serviceId }, ownerId);
-    res.status(201).json(createdListing);
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post('/newListing', authenticateHomeowner, createListing);
 
 module.exports = router;
