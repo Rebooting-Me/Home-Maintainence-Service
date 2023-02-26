f0aab3e4c81b6669f4228be5dea921fb0c5ddebb
Rebooting-Me <arsharma@ucsd.edu>
2023-02-26 10:27:59 -0800

    some pre-checks

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,12 @@ const app = require('./app');
 require('dotenv').config();
 
 // Database
-const databaseUri = process.env.MONGODB_URI;
-mongoose.connect(databaseUri, { useNewUrlParser: true })
+const db = process.env.MONGO_URI;
+mongoose.connect(db, { useNewUrlParser: true })
   .then(() => {
     console.log("MongoDB is connected");
-    // Use 6000 as the default port
-    const port = process.env.PORT || 6000;
+    // Use 3000 as the default port
+    const port = process.env.PORT || 3000;
     app.listen(port, () => console.log(`Server is listening on port ${port} !`))
   })
   .catch(error => console.log(error));
