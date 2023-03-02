4d2b27acf6e4854edf7cf72654b4f5ca1c9a7343
Rebooting-Me <arsharma@ucsd.edu>
2023-03-02 12:15:44 -0800

    successfully implemented the filter functionality for multiple-view-listings on front end

--- client/src/components/listings/Listings.jsx
+++ client/src/components/listings/Listings.jsx
@@ -4,22 +4,25 @@ import { useAuthContext } from '../../hooks/useAuthContext';
 const Listings = () => {
   const [listings, setListings] = useState([]);
   const { user } = useAuthContext();
-    console.log('Refresh: ', )
+  const [filter, setFilter] = useState({});
 
-    useEffect(() => {
-        fetch('http://localhost:3001/api/contractor/listings', {
-            method: 'POST',
-            headers: {
-              'Authorization': `Bearer ${user.token}`
-            }
-          })
-          .then(response => response.json())
-          .then(data => setListings(data))
-          .catch(error => console.error(error));
-      }, []);
+  useEffect(() => {
+    fetch('/api/contractor/listings', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${user.token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(filter)
+    })
+    .then(response => response.json())
+    .then(data => setListings(data))
+    .catch(error => console.error(error));
+  }, [filter]);
 
   return (
     <div>
+      <ListingFilters setFilter={setFilter} />
       {listings.map(listing => (
         <ListingCard
           key={listing.id}
@@ -29,6 +32,46 @@ const Listings = () => {
         />
       ))}
     </div>
+  );
+};
+
+const ListingFilters = ({ setFilter }) => {
+  const [state, setState] = useState('');
+  const [city, setCity] = useState('');
+  const [zip, setZip] = useState('');
+  const [services, setServices] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const filter = {};
+
+    if (state) {
+      filter.state = state;
+    }
+
+    if (city) {
+      filter.city = city;
+    }
+
+    if (zip) {
+      filter.zip_code = zip;
+    }
+
+    if (services) {
+      filter.services = services.split(',').map(service => service.trim());
+    }
+
+    setFilter(filter);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="State" value={state} onChange={e => setState(e.target.value)} />
+      <input type="text" placeholder="City" value={city} onChange={e => setCity(e.target.value)} />
+      <input type="text" placeholder="Zip Code" value={zip} onChange={e => setZip(e.target.value)} />
+      <input type="text" placeholder="Services" value={services} onChange={e => setServices(e.target.value)} />
+      <button type="submit">Filter</button>
+    </form>
   );
 };
 
