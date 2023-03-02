03e2bdb056616a20d84564bbc94818a5be8c95a6
Rebooting-Me <arsharma@ucsd.edu>
2023-03-02 15:41:30 -0800

    Successfully styled the multiple-listings-view

--- client/src/components/listings/Listings.jsx
+++ client/src/components/listings/Listings.jsx
@@ -1,5 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useAuthContext } from '../../hooks/useAuthContext';
+import styles from './Listings.module.css';
+import imageUrl from './image.svg';
+import Filter from './Filter.svg'
+import ServiceIcon from './ServiceIcon';
+// import plumbing from '../../assets/plumbing.svg';
+// import electrical from '../../assets/electrical.svg';
+// import landscaping from '../../assets/landscaping.svg';
+// import roofing from '../../assets/roofing.svg';
+// import pest from '../../assets/pest.svg';
+// import remodeling from '../../assets/remodeling.svg';
 
 const Listings = () => {
   const [listings, setListings] = useState([]);
@@ -65,26 +75,31 @@ const ListingFilters = ({ setFilter }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className= { styles.filter } onSubmit={handleSubmit}>
       <input type="text" placeholder="State" value={state} onChange={e => setState(e.target.value)} />
       <input type="text" placeholder="City" value={city} onChange={e => setCity(e.target.value)} />
       <input type="text" placeholder="Zip Code" value={zip} onChange={e => setZip(e.target.value)} />
       <input type="text" placeholder="Services" value={services} onChange={e => setServices(e.target.value)} />
-      <button type="submit">Filter</button>
+      <button type="submit" className={styles.filterButton}><img src={ Filter } /></button>
     </form>
   );
 };
 
 const ListingCard = ({ title, description, services }) => {
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <ul>
-        {services.map(service => (
-          <li key={service}>{service}</li>
-        ))}
-      </ul>
+    <div className={styles.listingCard}>
+        <div className={styles.listingImageWrapper}>
+            <img src={imageUrl} alt={title} className={styles.listingImage} />
+        </div>
+        <div className={styles.listingDetails }>
+            <h1>{title}</h1>
+            <ul>
+                {services.map(service => (
+                    <li key={service}><ServiceIcon service={service} />{service}</li>
+                ))}
+            </ul>
+            <p>{description} <span>... more</span></p>
+        </div>
     </div>
   );
 };
